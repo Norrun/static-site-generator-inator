ab82fbccae34d36b5d8466dfaf384c6d2f52bf3a
Norrun <trulvens@tutanota.com>
2025-10-09 00:26:22 +0200

    text splitting, needs more tests!

---
+++ src/test_general.py
@@ -0,0 +1,13 @@
+from main import split_nodes_delimiter
+from textnode import TextNode, TextType
+import unittest
+
+class TestNodeSplit(unittest.TestCase):
+    def test_first(self):
+        node = TextNode("This is text with a **bolded phrase** in the middle",TextType.PLAIN)
+        split = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(split,[
+    TextNode("This is text with a ", TextType.PLAIN),
+    TextNode("bolded phrase", TextType.BOLD),
+    TextNode(" in the middle", TextType.PLAIN),
+])
