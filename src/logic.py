c6101e8f82f7ac75d7eccf83e8bce98ad620bfb7
Norrun <trulvens@tutanota.com>
2025-10-09 01:21:44 +0200

    splitting links and images. Links need more work. Needs more tests. Will probably brake in the future.

--- src/logic.py
+++ src/logic.py
@@ -1,4 +1,5 @@
 import re
+from textnode import TextNode, TextType
 
 def extract_markdown_images(text):
     matches = re.findall(r"!\[(.*?)\]\((.*?)\)", text)
@@ -7,3 +8,39 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     matches = re.findall(r"\[(.*?)\]\((.*?)\)", text)
     return matches
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        matches = extract_markdown_images(node.text)
+        if len(matches) == 0:
+            new_nodes.append(node)
+            continue
+        next_section = node.text
+        for m in matches:
+            sections = next_section.split(f"![{m[0]}]({m[1]})", 1)
+            if sections[0] != "" :
+                new_nodes.append(TextNode(sections[0], TextType.PLAIN))
+            new_nodes.append(TextNode(m[0],TextType.IMAGE,m[1]))
+            if sections[1] != "" :
+                next_section = sections[1]
+        #new_nodes.append(next_section)
+    return new_nodes
+
+
+def split_nodes_links(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        matches = extract_markdown_links(node.text)
+        if len(matches) == 0:
+            new_nodes.append(node)
+            continue
+        for m in matches:
+            sections = node.text.split(f"[{m[0]}]({m[1]})", 1)
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.PLAIN))
+            new_nodes.append(TextNode(m[0],TextType.LINK,m[1]))
+            if sections[1] != "":
+                new_nodes.append(TextNode(sections[1], TextType.PLAIN))
+    return new_nodes
--- src/test_logic.py
+++ src/test_logic.py
@@ -1,5 +1,7 @@
 import unittest
 import logic
+from textnode import TextNode, TextType
+
 
 class TestLogic(unittest.TestCase):
     def test_regex_image(self):
@@ -8,3 +10,21 @@ class TestLogic(unittest.TestCase):
     def test_regex_link(self):
         matches = logic.extract_markdown_links("This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)")
         self.assertListEqual(matches,[("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")])
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.PLAIN,
+        )
+        new_nodes = logic.split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.PLAIN),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.PLAIN),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
