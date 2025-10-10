61f167c37cf08b454b31265fef451e6f400a131b
Norrun <trulvens@tutanota.com>
2025-10-10 20:41:55 +0200

    WIP text to text nodes

--- src/logic.py
+++ src/logic.py
@@ -44,3 +44,31 @@ def split_nodes_links(old_nodes):
             if sections[1] != "":
                 new_nodes.append(TextNode(sections[1], TextType.PLAIN))
     return new_nodes
+
+def split_nodes_delimiter(old_nodes: list[TextNode], delimiter, text_type: TextType):
+    new_nodes = []
+    for node in old_nodes:
+        print(node)
+        if node.text_type != TextType.PLAIN:
+            new_nodes.append(node)
+            continue
+        texts = node.text.split(delimiter)
+        if len(texts) == 1:
+            new_nodes.append(node)
+        if len(texts) % 3 != 0:
+            raise Exception("invalid markdown syntax")
+        for i in range(0, len(texts)):
+            if i % 2 == 0:
+                if texts[i] != "":
+                    new_nodes.append(TextNode(texts[i], TextType.PLAIN))
+            else:
+                new_nodes.append(TextNode(texts[i],text_type))
+    return new_nodes
+
+def text_to_textnodes(text):
+    nodes = split_nodes_delimiter([TextNode(text, text_type=TextType.PLAIN)],"**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes,"_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes,"`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_links(nodes)
+    return nodes
