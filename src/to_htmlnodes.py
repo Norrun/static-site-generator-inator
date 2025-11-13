ee8a85dae9188af3a9d5332637f97697978bc488
Norrun <trulvens@tutanota.com>
2025-11-13 23:05:53 +0100

    WIP to_htmlNode, create heading and create code implemented.

--- src/to_htmlnodes.py
+++ src/to_htmlnodes.py
@@ -1,5 +1,6 @@
 from parentnode import ParentNode
 from leafnode import LeafNode
+from textnode import TextNode, TextType
 from blocks import BlockType, markdown_to_blocks, block_to_blocktype
 from text_logic import text_to_textnodes
 from textnode import text_node_to_html_node
@@ -30,13 +31,27 @@ def create_heading(heading_block):
     if m is None:
         raise ValueError("invalid heading block")
     level = len(m[0])
+    children = process_leafs(m[0][level + 1 :])
+    tag = f"h{level}"
+    return ParentNode(tag,children)
+
 
 def create_code(code_block: str):
-    pass
+    lines =  code_block.split("\n")
+    syntax_removed = lines[1:-1]
+    value = "\n".join(syntax_removed)
+    text_node = TextNode(value,TextType.PLAIN)
+    inner_node = text_node_to_html_node(text_node)
+    code = ParentNode("code",[inner_node])
+    pre = ParentNode("pre",[code])
+    return pre
+
 
 def create_quote(quote: str):
-    value = quote.replace(">","")
+    value = quote.replace("> ","")
+
 
 def process_leafs(content):
     tnodes = text_to_textnodes(content)
     return list(map(text_node_to_html_node,tnodes))
+
